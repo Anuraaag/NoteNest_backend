54e38ae7b39128e4a39a6833e67e7bbad4e23917
Anuraaag <anuraggupta10000@gmail.com>
2022-03-17 01:20:15 +0530

    storing pw as salted hash in db, create and send jwt token on user creation

--- routes/auth.js
+++ routes/auth.js
@@ -2,37 +2,55 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User') // import a model that's to be used, just like in laravel-8
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken");
 
 //second arg contains the rules
 router.post('/', [
-    body('name', 'Enter a valid name').isLength({min: 3}),
+    body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be at least 5 characters').isLength({min: 5})
-], 
+    body('password', 'Password must be at least 5 characters').isLength({ min: 5 })
+],
 
-(req, res) => {
+    async (req, res) => {
 
-    //Checking for error based on aforementioned rules
-    const errors = validationResult(req)
-    if(!errors.isEmpty()) {
-        return res.status(400).json({error: errors.array() })
-    }
+        //Checking for error based on aforementioned rules
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ error: errors.array() })
+        }
 
-    //Creating user 
-    // try {
-     
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
-        }).then(user => res.json(user))
-        .catch(error => res.status(400).json({error: error }))
-    
-    // } catch (error) {
-        
-    // }
+        //Checking if user already exists
+        let user = await User.findOne({ email: req.body.email }) // db crud is async
+        if (user) {
+            return res.status(400).json({ 'error': "This email is already taken" })
+        }
 
-    // res.send(req.body)
-})
+        //Creating user 
+        try {
+
+            const salt = await bcrypt.genSalt()
+            const hash = await bcrypt.hash(req.body.password, salt)
+
+            user = await User.create({ // db crud is async
+                name: req.body.name,
+                email: req.body.email,
+                password: hash
+            })
+
+            // as user gets created, we create and send a JWT token to the client
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            const JWT_Secret = "fg7erfgt543r%^$#"
+            const jwtToken = jwt.sign(payload, JWT_Secret)
+            res.json({'authToken': jwtToken})
+
+        } catch (error) {
+            res.status(400).json({ 'error': error })
+        }
+    })
 
 module.exports = router
