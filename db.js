57817265048cdeb48dc9533398fc377d0a69b65d
Anuraaag <anuraggupta10000@gmail.com>
2022-03-22 22:43:46 +0530

    mongo uri

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoUri = "mongodb://localhost:27017/notenest?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
-mongoUri = "mongodb+srv://AnuragGupta:The1@ndonly#)@notenest.okifm.mongodb.net/NoteNest?retryWrites=true&w=majority"
+mongoUri = "mongodb+srv://AnuragGupta:The1%40ndonly%23%29@notenest.okifm.mongodb.net/NoteNest?retryWrites=true&w=majority"
 
 const connectToMongo = () => {
     mongoose.connect(mongoUri, () => {
